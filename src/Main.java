b28304f3c5721a6ca97fdf05c8ac6bd55f37df54
Johana <johana.quiroga@alumnos.frm.utn.edu.ar>
2024-08-30 16:32:57 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ingrese su nombre: ");
+    String nombre = sc.nextLine();
+    System.out.println("Su nombre es: " + nombre);
+        }
+    }
